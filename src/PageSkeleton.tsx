1a9df64b510e22990a8c2bd6d0bdc9b2c469bc5f
tchardin <tdotchardin@gmail.com>
2021-01-11 14:16:22 +0100

    Improve chunk loading state

--- src/PageSkeleton.tsx
+++ src/PageSkeleton.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import styles from './Home.module.css';
 
+import ActivityIndicator from './components/ActivityIndicator';
+
 export default function PageSkeleton() {
   return (
     <main className={styles.overviewContainer}>
       <section className={`${styles.section} ${styles.sectionHero}`}>
-        <div className={styles.sectionContent}></div>
+        <div
+          className={`${styles.sectionContent} ${styles.sectionPlaceholder}`}>
+          <ActivityIndicator />
+        </div>
       </section>
     </main>
   );
